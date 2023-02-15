458bff4b5979e694070d01db99b68228479ea053
Andrew <acastillonevarez@bsd220.org>
2023-02-15 09:30:42 -0600

    Initial Commit

---
+++ 'Za Hunter/_Za_HunterApp.swift
@@ -0,0 +1,17 @@
+//
+//  _Za_HunterApp.swift
+//  'Za Hunter
+//
+//  Created by Andrew Castillo Nevarez on 2/15/23.
+//
+
+import SwiftUI
+
+@main
+struct _Za_HunterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
